fc0ff6c912408fbc3d0882749dc51ad4d6c4ccce
Miriam Landau <miriamlandau1999@gmail.com>
2017-09-10 00:08:12 -0400

    Initial Commit

---
+++ VueWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VueWebApplication.MvcApplication" Language="C#" %>
---
+++ VueWebApplication/Scripts/index.js
@@ -0,0 +1,14 @@
+﻿new Vue({
+    el: '#app',
+    data: {
+        word: '',
+        words: ["hello"],
+    },
+
+    methods:{
+        addWord: function () {
+            this.words.push(this.word);
+            this.word ='';
+        }
+    }
+});
